f53b48682bee2a902aea94ba30c61e0bd1fd7514
Dave Wood <dave@cerebralgardens.com>
2014-06-22 01:12:57 -0400

    Add unit tests to test defaultInstance always returns the same instance

--- XCGLogger/XCGLoggerTests/XCGLoggerTests.swift
+++ XCGLogger/XCGLoggerTests/XCGLoggerTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import XCGLogger
 
 class XCGLoggerTests: XCTestCase {
     
@@ -20,16 +21,37 @@ class XCGLoggerTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    //    func testExample() {
+    //        // This is an example of a functional test case.
+    //        XCTAssert(true, "Pass")
+    //    }
+    //    
+    //    func testPerformanceExample() {
+    //        // This is an example of a performance test case.
+    //        self.measureBlock() {
+    //            // Put the code you want to measure the time of here.
+    //        }
+    //    }
+
+    func testDefaultInstance() {
+        // Test that if we request the default instance multiple times, we always get the same instance
+        let defaultInstance1: XCGLogger = XCGLogger.defaultInstance()
+        defaultInstance1.identifier = "com.cerebralgardens.xcglogger.defaultInstance"
+
+        let defaultInstance2: XCGLogger = XCGLogger.defaultInstance()
+        defaultInstance2.identifier = "com.cerebralgardens.xcglogger.defaultInstance.second" // this should also change defaultInstance1.identifier
+
+        XCTAssert(defaultInstance1.identifier == defaultInstance2.identifier, "Fail: defaultInstance() is not returning a common instance")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
+
+    func testDistinctInstances() {
+        // Test that if we request the multiple instances, we get different instances
+        let instance1: XCGLogger = XCGLogger()
+        instance1.identifier = "instance1"
+
+        let instance2: XCGLogger = XCGLogger()
+        instance2.identifier = "instance2" // this should not affect instance1
+
+        XCTAssert(instance1.identifier != instance2.identifier, "Fail: same instance is being returned")
     }
-    
 }
