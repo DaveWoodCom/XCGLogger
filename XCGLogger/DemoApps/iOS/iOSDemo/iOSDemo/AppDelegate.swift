e030da8a4c0113778656a59310314168309a1ac7
Dave Wood <dave@cerebralgardens.com>
2015-04-14 01:36:22 -0400

    Add a global noop() function for examples (also useful for doing nothing on a specific switch option)

--- XCGLogger/DemoApps/iOS/iOSDemo/iOSDemo/AppDelegate.swift
+++ XCGLogger/DemoApps/iOS/iOSDemo/iOSDemo/AppDelegate.swift
@@ -60,6 +60,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
 }
 
+func noop() {
+    // Global no operation function, useful for doing nothing in a switch option, and examples
+}
