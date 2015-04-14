026c7d6e52ffbc25303763fbc72e48a50f4936b4
Dave Wood <dave@cerebralgardens.com>
2015-04-14 04:58:28 -0400

    Add Podspec, version bump to 2.0 (Swift 1.2 now required)

--- XCGLogger/Library/XCGLogger/XCGLogger.swift
+++ XCGLogger/Library/XCGLogger/XCGLogger.swift
@@ -261,7 +261,7 @@ public class XCGLogger : DebugPrintable {
         public static let baseFileLogDestinationIdentifier = "com.cerebralgardens.xcglogger.logdestination.file"
         public static let nsdataFormatterCacheIdentifier = "com.cerebralgardens.xcglogger.nsdataFormatterCache"
         public static let logQueueIdentifier = "com.cerebralgardens.xcglogger.queue"
-        public static let versionString = "1.9"
+        public static let versionString = "2.0"
     }
 
     // MARK: - Enums
