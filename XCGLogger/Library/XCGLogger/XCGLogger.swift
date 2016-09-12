48aa4d101eb1069ce5778c5133d6aef3c1c7a348
David Whetstone <david@humblehacker.com>
2016-09-12 08:55:25 -0700

    update projects and library for swift 2.3
    I started with @bersaelor's swift_2.3 branch, rebased against
    @DaveWoodCom's master, and made one small change to get it to build with
    Xcode 8 GM.

--- XCGLogger/Library/XCGLogger/XCGLogger.swift
+++ XCGLogger/Library/XCGLogger/XCGLogger.swift
@@ -440,7 +440,7 @@ public class XCGFileLogDestination: XCGBaseLogDestination {
                 return
             }
 
-            let logDetails = XCGLogDetails(logLevel: .Info, date: NSDate(), logMessage: "XCGLogger " + (fileExists && shouldAppend ? "appending" : "writing") + " log to: " + writeToFileURL.absoluteString, functionName: "", fileName: "", lineNumber: 0)
+            let logDetails = XCGLogDetails(logLevel: .Info, date: NSDate(), logMessage: "XCGLogger " + (fileExists && shouldAppend ? "appending" : "writing") + " log to: " + writeToFileURL.absoluteString!, functionName: "", fileName: "", lineNumber: 0)
             owner._logln(logDetails.logMessage, logLevel: logDetails.logLevel)
             if owner.logDestination(identifier) == nil {
                 processInternalLogDetails(logDetails)
