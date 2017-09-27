f087302e99eda08f204d02c962b8caab0df53a5e
Dave Wood <dave@cerebralgardens.com>
2017-09-27 04:14:47 -0400

    Fix infinite loop when logging to a file and we run out of drive space

--- Sources/XCGLogger/Destinations/FileDestination.swift
+++ Sources/XCGLogger/Destinations/FileDestination.swift
@@ -107,7 +107,7 @@ open class FileDestination: BaseQueuedDestination {
                         self.logFileHandle?.write(encodedData)
                     },
                     catch: { (exception: NSException) in
-                        owner._logln("Objective-C Exception occurred: \(exception)", level: .error, source: self)
+                        print("Objective-C Exception occurred: \(exception)", level: .error, source: self)
                     })
                 }
             }
@@ -238,7 +238,7 @@ open class FileDestination: BaseQueuedDestination {
                 self.logFileHandle?.write(encodedData)
             },
             catch: { (exception: NSException) in
-                self.owner?._logln("Objective-C Exception occurred: \(exception)", level: .error, source: self)
+                print("Objective-C Exception occurred: \(exception)", level: .error, source: self)
             })
         }
     }
