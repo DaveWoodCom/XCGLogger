1ca0f1769494468f9e669717773b73b320a1165d
Dave Wood <dave@cerebralgardens.com>
2017-09-28 06:10:10 -0400

    Update iOS demo to use the advanced configuration (old `setup` style still there as an example)

--- DemoApps/iOSDemo/iOSDemo/AppDelegate.swift
+++ DemoApps/iOSDemo/iOSDemo/AppDelegate.swift
@@ -12,19 +12,24 @@ import XCGLogger
 
 let appDelegate = UIApplication.shared.delegate as! AppDelegate
 let log: XCGLogger = {
-    // Setup XCGLogger
-    let log = XCGLogger.default
 
 #if USE_NSLOG // Set via Build Settings, under Other Swift Flags
+    // Setup XCGLogger
+    let log = XCGLogger.default
     log.remove(destinationWithIdentifier: XCGLogger.Constants.baseConsoleDestinationIdentifier)
     log.add(destination: AppleSystemLogDestination(identifier: XCGLogger.Constants.systemLogDestinationIdentifier))
     log.logAppDetails()
-#else
+
+#elseif USE_OLD_SETUP_STYLE
+    // This old style where we us a single setup method isn't recommended for production apps.
+    // It's mostly useful for when you need a quick and dirty log, in a sample project, or unit test etc
+    let log = XCGLogger.default
     let logPath: URL = appDelegate.cacheDirectory.appendingPathComponent("XCGLogger_Log.txt")
     log.setup(level: .debug, showThreadName: true, showLevel: true, showFileNames: true, showLineNumbers: true, writeToFile: logPath)
 
     // Add colour (using the ANSI format) to our file log, you can see the colour when `cat`ing or `tail`ing the file in Terminal on macOS
     // This is mostly useful when testing in the simulator, or if you have the app sending you log files remotely
+    // This is also an example of how you can pull a log destination out of the logger later to alter it's setup
     if let fileDestination: FileDestination = log.destination(withIdentifier: XCGLogger.Constants.fileDestinationIdentifier) as? FileDestination {
         let ansiColorLogFormatter: ANSIColorLogFormatter = ANSIColorLogFormatter()
         ansiColorLogFormatter.colorize(level: .verbose, with: .colorIndex(number: 244), options: [.faint])
@@ -50,6 +55,63 @@ let log: XCGLogger = {
     //    xcodeColorsLogFormatter.colorize(level: .severe, with: .white, on: .red)
     //    consoleDestination.formatters = [xcodeColorsLogFormatter]
     //}
+#else
+    // Setup XCGLogger (Advanced/Recommended Usage)
+    // Create a logger object with no destinations
+    let log = XCGLogger(identifier: "advancedLogger", includeDefaultDestinations: false)
+
+    // Create a destination for the system console log (via NSLog)
+    let systemDestination = AppleSystemLogDestination(identifier: "advancedLogger.appleSystemLogDestination")
+
+    // Optionally set some configuration options
+    systemDestination.outputLevel = .debug
+    systemDestination.showLogIdentifier = false
+    systemDestination.showFunctionName = true
+    systemDestination.showThreadName = true
+    systemDestination.showLevel = true
+    systemDestination.showFileName = true
+    systemDestination.showLineNumber = true
+
+    // Add the destination to the logger
+    log.add(destination: systemDestination)
+
+    // Create a file log destination
+    let logPath: URL = appDelegate.cacheDirectory.appendingPathComponent("XCGLogger_Log.txt")
+    let autoRotatingFileDestination = AutoRotatingFileDestination(writeToFile: logPath, identifier: "advancedLogger.fileDestination", shouldAppend: true,
+                                                                  attributes: [.protectionKey: FileProtectionType.completeUntilFirstUserAuthentication], // Set file attributes on the log file
+                                                                  maxFileSize: 1024 * 5, // 5k, not a good size for production (default is 1 megabyte)
+                                                                  maxTimeInterval: 60) // 1 minute, also not good for production (default is 10 minutes)
+
+    // Optionally set some configuration options
+    autoRotatingFileDestination.outputLevel = .debug
+    autoRotatingFileDestination.showLogIdentifier = false
+    autoRotatingFileDestination.showFunctionName = true
+    autoRotatingFileDestination.showThreadName = true
+    autoRotatingFileDestination.showLevel = true
+    autoRotatingFileDestination.showFileName = true
+    autoRotatingFileDestination.showLineNumber = true
+    autoRotatingFileDestination.showDate = true
+    autoRotatingFileDestination.targetMaxLogFiles = 10 // probably good for this demo and production, (default is 10, max is 255)
+
+    // Process this destination in the background
+    autoRotatingFileDestination.logQueue = XCGLogger.logQueue
+
+    // Add colour (using the ANSI format) to our file log, you can see the colour when `cat`ing or `tail`ing the file in Terminal on macOS
+    let ansiColorLogFormatter: ANSIColorLogFormatter = ANSIColorLogFormatter()
+    ansiColorLogFormatter.colorize(level: .verbose, with: .colorIndex(number: 244), options: [.faint])
+    ansiColorLogFormatter.colorize(level: .debug, with: .black)
+    ansiColorLogFormatter.colorize(level: .info, with: .blue, options: [.underline])
+    ansiColorLogFormatter.colorize(level: .warning, with: .red, options: [.faint])
+    ansiColorLogFormatter.colorize(level: .error, with: .red, options: [.bold])
+    ansiColorLogFormatter.colorize(level: .severe, with: .white, on: .red)
+    autoRotatingFileDestination.formatters = [ansiColorLogFormatter]
+
+    // Add the destination to the logger
+    log.add(destination: autoRotatingFileDestination)
+
+    // Add basic app info, version info etc, to the start of the logs
+    log.logAppDetails()
+
 #endif
 
     // You can also change the labels for each log level, most useful for alternate languages, French, German etc, but Emoji's are more fun
