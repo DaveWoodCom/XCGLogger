da962de79eea3be305f8f8e3f66752c3a7955afd
Brian Buck <bbuck911@gmail.com>
2020-04-07 17:28:04 -0700

    added type: .dynamic

--- Package.swift
+++ Package.swift
@@ -9,9 +9,11 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "XCGLogger",
+            type: .dynamic,
             targets: ["XCGLogger"]),
         .library(
             name: "ObjcExceptionBridging",
+            type: .dynamic,
             targets: ["ObjcExceptionBridging"]),
     ],
     dependencies: [
