2bdd84adad73fbc8b18e47966b1bbf94054f3f9e
Noam Tamim <noam.tamim@kaltura.com>
2020-07-19 22:18:03 +0300

    Update Package.swift
    Add platforms support

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "XCGLogger",
+    platforms: [
+        .iOS(.v8), .tvOS(.v9), .watchOS(.v2)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
