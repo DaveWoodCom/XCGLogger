330da5878aa5ede4377c3051267fc892e36af59b
Rinki Etheridge <rinki.etheridge@favordelivery.com>
2024-04-16 15:14:27 -0500

    [DOS-6868] Added Privacy Manifest

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -23,7 +23,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "XCGLogger",
-            dependencies: ["ObjcExceptionBridging"]),
+            dependencies: ["ObjcExceptionBridging"],
+            path: "Sources/XCGLogger",
+            resources: [
+                .process("PrivacyInfo.xcprivacy")
+            ]
+        ),
         .testTarget(
             name: "XCGLoggerTests",
             dependencies: ["XCGLogger"]),
