a65895c48d159aefde53511c69a9426f3d20abcc
Dave Wood <dave@cerebralgardens.com>
2024-04-29 00:47:57 -0400

    chore: version bump 7.1.2

--- Sources/XCGLogger/XCGLogger.swift
+++ Sources/XCGLogger/XCGLogger.swift
@@ -46,7 +46,7 @@ open class XCGLogger: CustomDebugStringConvertible {
         public static let userInfoKeyInternal = "\(baseIdentifier).internal"
 
         /// Library version number
-        public static let versionString = "7.1.1"
+        public static let versionString = "7.1.2"
 
         /// Internal userInfo
         internal static let internalUserInfo: [String: Any] = [XCGLogger.Constants.userInfoKeyInternal: true]
